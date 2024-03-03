538290dd5e1ece5ecf69624fbb61079790899abb
jpcadinelli <jpcadinelli@gmail.com>
2024-03-03 09:59:50 -0300

    Aula 4 finalizada

---
+++ app.py
@@ -0,0 +1,19 @@
+from modelos.restaurante import Restaurante
+
+
+restaurante_praca = Restaurante('praça', 'gourmet')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Emy', 5)
+
+restaurante_pizza = Restaurante('pizza express', 'italiana')
+restaurante_mexicano = Restaurante('Mexican Food', 'mexicana')
+restaurante_japones = Restaurante('japa', 'japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
